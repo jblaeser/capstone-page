5fd63216b4f0ee174f64e900949fbb9657e88545
jblaeser <44827644+jblaeser@users.noreply.github.com>
2020-11-14 16:33:36 -0600

    problem statement section timing

--- js/script1.js
+++ js/script1.js
@@ -10,7 +10,7 @@ $(function () { // wait for document ready
     // define movement of panels
     var wipeAnimation = new TimelineMax()
         .fromTo("section.panel.two", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-        .fromTo("section.panel.three",    1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone})  // in from right
+        .fromTo("section.panel.three",    1, {y:  "100%"}, {y: "-30%", ease: Linear.easeNone})  // in from right
         .fromTo("section.panel.four", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
         .fromTo("section.panel.five", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})  // in from left
         .fromTo("section.panel.six",    1, {x:  "100%"}, {x: "-50%", ease: Linear.easeNone})  // in from right
@@ -30,16 +30,22 @@ $(function () { // wait for document ready
         .addIndicators() // add indicators (requires plugin)
         .addTo(controller);
 
-
-
         new ScrollMagic.Scene({
             triggerElement: "#trigger1",
-            triggerHook: 0.9, // show, when scrolled 10% into view
-            duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+            triggerHook: 0.40, // show, when scrolled 10% into view
+            duration: "30%", // hide 10% before exiting view (80% + 10% from bottom)
             offset: 50 // move trigger to center of element
         })
         .setClassToggle("#reveal1", "visible") // add class to reveal
-        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+        new ScrollMagic.Scene({
+            triggerElement: "#trigger2",
+            triggerHook: 0.65, // show, when scrolled 10% into view
+            duration: "30%", // hide 10% before exiting view (80% + 10% from bottom)
+            offset: 50 // move trigger to center of element
+        })
+        .setClassToggle("#reveal2", "visible") // add class to reveal
         .addTo(controller);
 });
 
