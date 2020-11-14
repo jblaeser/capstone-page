fb4e9649a857eb3d26d2faa4a79c6dd57e50d70e
jblaeser <44827644+jblaeser@users.noreply.github.com>
2020-11-13 19:45:21 -0600

    section directions

--- js/script1.js
+++ js/script1.js
@@ -4,14 +4,14 @@ $(function () { // wait for document ready
 
     // define movement of panels
     var wipeAnimation = new TimelineMax()
-        .fromTo("section.panel.two", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-        .fromTo("section.panel.three",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-        .fromTo("section.panel.four", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
-        .fromTo("section.panel.five", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+        .fromTo("section.panel.two", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+        .fromTo("section.panel.three",    1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone})  // in from right
+        .fromTo("section.panel.four", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
+        .fromTo("section.panel.five", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})  // in from left
         .fromTo("section.panel.six",    1, {x:  "100%"}, {x: "-50%", ease: Linear.easeNone})  // in from right
         .fromTo("section.panel.seven", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone}) // in from top
-        .fromTo("section.panel.eight",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-        .fromTo("section.panel.nine", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+        .fromTo("section.panel.eight",    1, {y:  "100%"}, {y: "0%", ease: Linear.easeNone})  // in from right
+        .fromTo("section.panel.nine", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
 
     // create scene to pin and link animation
     new ScrollMagic.Scene({
